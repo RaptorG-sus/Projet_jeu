3b32b904d616b2a415207f9215cc723bc36f259d
RaptorG-sus <quentin.peschard38@gmail.com>
2025-05-03 17:21:13 +0200

    0.0.2.4

--- scripts/scripts_control/card_button/card_button.gd
+++ scripts/scripts_control/card_button/card_button.gd
@@ -1,20 +1,30 @@
 extends Button
 
+@onready var number_card = 0
+
+var card_data = GameData.card_data
+
 func _on_pressed() -> void:
 
-	var liste_buyed = $"../../../../..".liste_buyed
-	var liste_generer = $"../../../..".liste_generer
-	var liste_price = $"../../../..".liste_price
+	var liste_buyed = $"../../../..".liste_buyed
+	var liste_generer = $"../../..".liste_generer
+	var liste_price = $"../../..".liste_price
 
 
-	if liste_buyed[int(name)-1]:
-		liste_buyed[int(name)-1] = 0
-		$".."/card_label.text = liste_generer[int(name)-1] + ' ' + str(liste_price[int(name)-1])
-		$"../../../../..".sum_price -= liste_price[int(name)-1]
-		$"../../../.."/tot_price.text = str($"../../../../..".sum_price)
+	if liste_buyed[number_card]:
+		liste_buyed[number_card] = 0
+		$".."/card_label.text = card_data[liste_generer[number_card]]["text"] + ' ' + str(liste_price[number_card])
+		$"../../../..".sum_price -= liste_price[number_card]
+		$"../../.."/tot_price.text = str($"../../../..".sum_price)
+		$".."/AnimationSelect.play("card_unselect")
+		$".."/AnimationShake.stop()
+
 	else:
-		liste_buyed[int(name)-1] = 1
+		liste_buyed[number_card] = 1
 		$".."/card_label.text = "buyed"
-		$"../../../../..".sum_price += liste_price[int(name)-1]
-		$"../../../.."/tot_price.text = str($"../../../../..".sum_price)
+		$"../../../..".sum_price += liste_price[number_card]
+		$"../../.."/tot_price.text = str($"../../../..".sum_price)
+		$".."/AnimationSelect.play("card_select")
+		$".."/AnimationShake.play("shake")
+
 	print(liste_buyed)
