adeae0a69bbf4e5ece3bbb932ffeb2c52b4259f4
Tandem-master <bechard.master@gmail.com>
2025-05-14 11:49:44 +0200

    BaseCode_before_D_and_D

--- scripts/scripts_card/card_overlay.gd
+++ scripts/scripts_card/card_overlay.gd
@@ -5,20 +5,21 @@ extends Button
 @onready var card_name :String
 var card_data = GameData.card_data
 
+signal mouse_in(vector: Vector2, click:bool, button_statment:bool)
+
 
 func _ready() -> void:
 	$card.texture = load(card_data[card_name]["texture"])
 	text = card_data[card_name]["text"]
 	$card.scale = Vector2(8,8)
 
-	
-func _mouse_enter() -> void:
 
+func _mouse_enter() -> void:
+	mouse_in.emit(get_global_mouse_position(), false, false)
 	$animation.play("test")
 	self.z_index += 5
 	$card.scale *= 2
 
-	
 
 func _mouse_exit() -> void:
 	
@@ -26,3 +27,11 @@ func _mouse_exit() -> void:
 	self.z_index -= 5
 	$card.scale /= 2
 	
+
+func _on_button_down() -> void:
+	mouse_in.emit(get_global_mouse_position(), true, false)
+	
+
+
+func _on_button_up() -> void:
+	mouse_in.emit(get_global_mouse_position(), true, true)
